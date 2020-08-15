6feaf097499432bec4093b2c7b787f58bb06b1c0
Tuseeq Raza <tuseeqraza1@gmail.com>
2020-08-16 00:32:03 +0500

    developer branch created
    Please only push in developer branch

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
-import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 
-
-export default class App extends Component
-{
-  render()
-  {
-    return(
+export default class App extends Component {
+  render() {
+    return (
       <View>
-        <Text>iiiaaaaaaaaaaa
-        </Text>
+        <Text>Please only push in developer branch</Text>
       </View>
     );
   }
