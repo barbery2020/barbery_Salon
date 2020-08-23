590c8d6c7c3172e7dfb6e070e43f22ef4108c9da
Tuseeq Raza <sp17-bcs-079@isbstudent.comsats.edu.pk>
2020-08-24 01:51:45 +0500

    002_servicedetails

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 import ViewImageScreen from './src/screen/BarberSide/ViewImageScreen';
+import ServiceDetailsScreen from './src/screen/BarberSide/ServiceDetailsScreen';
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ServiceDetailsScreen />;
 }
