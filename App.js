35646576f99d0f758fdf56bc1e085cd825b983ae
abdullahsaeed98 <56201080+abdullahsaeed98@users.noreply.github.com>
2020-08-25 19:29:44 +0500

    chat-done

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+// Import the screens
+import Main from './components/Main';
+import Chat from './components/Chat';
+import {createAppContainer} from 'react-navigation';
+// Import React Navigation
+import { createStackNavigator } from 'react-navigation-stack'
 
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Please only push in developer branch</Text>
-      </View>
-    );
-  }
-}
+// Create the navigator
+const navigator = createStackNavigator({
+  Main: { screen: Main },
+  Chat: { screen: Chat },
+});
+
+// Export it as the root component
+export default createAppContainer(navigator)
