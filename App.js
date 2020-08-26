7bd61122d02ac2777fb61e8df6e30dd6a0b9419f
Tuseeq Raza <64867680+tuseeqraza1@users.noreply.github.com>
2020-08-27 01:12:33 +0500

    Chat merge with barber managment (#2)
    * chat-done
    
    * chat 2.0
    
    * chat 3.0
    
    * chat src
    
    * chat 4.0
    
    Co-authored-by: abdullahsaeed98 <56201080+abdullahsaeed98@users.noreply.github.com>
    Co-authored-by: Tuseeq Raza <sp17-bcs-079@isbstudent.comsats.edu.pk>

--- App.js
+++ App.js
@@ -3,7 +3,11 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 import BarberStack from './src/components/BarberStack';
+import Chat from './src/screen/chat';
 
 export default function App() {
   return <BarberStack />;
+
 }
+
+export default App;
